0f9c3b26f99f87b0799f9d3131c3b53976fde2ee
Omar Faruk <omarfaruk@droitlab.com>
2023-07-05 15:30:22 +0600

    update

--- footer.php
+++ footer.php
@@ -8,6 +8,17 @@
  *
  * @package muffle
  */
+$footer_url = muffle_options('muffle_footer_background');
+$footer_background_url = MUFFLE_IMAGES.'/blog/banner/banner.png';
+
+if($footer_url && !empty($footer_url['url'])) {
+    $footer_background_url = $footer_url['url'];
+}
+
+if(!class_exists('Redux')){
+  $footer_background_url = '';
+}
+
 $footer_text = muffle_options('muffle_copyright_txt', 'Copyright &copy; 2022 <a href="#">DroitThemes</a> | All rights reserved');
 $footer_id = '';
 
@@ -18,7 +29,7 @@ if ( isset( $post->ID) && function_exists('header_footer_template_id')) {
 if ( $footer_id != '' && class_exists( '\Elementor\Plugin' ) ) {
     echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display( $footer_id );
 }else{ ?>
-    <footer id="colophon" class="site-footer">
+    <footer id="colophon" class="site-footer" data-bg-img="<?php echo esc_url($footer_background_url); ?>">
         <div class="site-info container">
             <?php echo wp_kses_post($footer_text); ?>
         </div><!-- .site-info -->
--- lib/options/opt_footer.php
+++ lib/options/opt_footer.php
@@ -17,6 +17,51 @@ Redux::set_section('muffle', array(
                 'teeny'         => false,
             )
         ),
+        array(
+            'title'     => esc_html__( 'Background Color', 'muffle' ),
+            'id'        => 'footer_bg_color',
+            'type'      => 'color',
+            'mode'      => 'background',
+            'output'    => array( '.site-footer' ),
+        ),
+        array(
+            'title'     => esc_html__( 'Footer Background', 'muffle' ),
+            'id'        => 'muffle_footer_background',
+            'type'      => 'media',
+        ),
+        array(
+            'id'          => 'footer_typo',
+            'type'        => 'typography',
+            'title'       => __('Typography', 'muffle'),
+            'google'      => true,
+            'font-backup' => true,
+            'color'       => false,
+            'output'      => array('.site-footer .site-info'),
+            'units'       => 'px',
+        ),
+        array(
+            'title'     => esc_html__( 'Font color', 'muffle' ),
+            'id'        => 'footer_font_color',
+            'type'      => 'color',
+            'output'    => array( '.site-footer .site-info' ),
+        ),
+        array(
+            'title'     => esc_html__( 'Font Link color', 'muffle' ),
+            'id'        => 'footer_link_font_color',
+            'type'      => 'color',
+            'output'    => array( '.site-footer .site-info a' ),
+        ),
+        array(
+            'title'     => esc_html__( 'Footer padding', 'muffle' ),
+            'subtitle'  => esc_html__( 'Padding around the menu action button.', 'muffle' ),
+            'id'        => 'footer_padding',
+            'type'      => 'spacing',
+            'output'    => array( '.site-footer' ),
+            'mode'      => 'padding',
+            'units'     => array( 'em', 'px', '%' ),      // You can specify a unit value. Possible: px, em, %
+            'units_extended' => 'true',
+            'required'  => array( 'is_menu_btn', '=', 'yes' )
+        ),
 	)
 ));
 
